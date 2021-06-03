3437013b32715cf6002835ba720edb3f71569afd
Rebecca Cremona <rcremona@law.harvard.edu>
2021-06-03 13:34:47 -0400

    Use postgres instead of mysql.

--- perma_web/fabfile/dev.py
+++ perma_web/fabfile/dev.py
@@ -172,14 +172,9 @@ def logs(log_dir=os.path.join(settings.PROJECT_ROOT, '../services/logs/')):
 
 
 @task
-def create_db(host='db', user='root', password='password'):
-    local("mysql -h {} -u{} -p{} -e 'create database perma character set utf8;'".format(host, user, password))
-
-
-@task
 def init_db():
     """
-        Run syncdb, apply migrations, and import fixtures for new dev database.
+        Apply migrations and import fixtures for new dev database.
     """
     local("python manage.py migrate")
     local("python manage.py loaddata fixtures/sites.json fixtures/users.json fixtures/folders.json")
